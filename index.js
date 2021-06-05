b76c3c6c05dc7264e09884693c21e19c46eee674
Abdur Rahim <rahimbd623@gmail.com>
2021-06-06 01:29:45 +0600

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+// Supports ES6
+// import { create, Whatsapp } from 'venom-bot';
+const venom = require('venom-bot');
+
+venom
+  .create()
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body === 'Hi' && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Welcome Abdur Rahim 🕷')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }else if (message.body === 'Rahim' && message.isGroupMsg === false) {
+     client
+        .sendText(message.from, 'I am Rahim Sarkar')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    } else {
+        client
+        .sendText(message.from, 'Wrong key')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+      }
+        
+  });
+}
